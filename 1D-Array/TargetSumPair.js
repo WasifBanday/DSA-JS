cf2f1a374d507fc96ffd9894b650ddd2cda26072
wasif_manzoor <wasifahmad679@gmail.com>
2025-11-07 21:08:41 +0530

    Tagget sum pair

--- 1D-Array/TargetSumPair.js
+++ 1D-Array/TargetSumPair.js
@@ -1,29 +1,31 @@
 // Find the total number of pairs in the Array whose sum is equal to the given value 'x'  ?
 
 let array = [2, 3, 4, 5, 6, 7, 8, 9]
-    // for (let i = 0; i < array.length; i++) {
-    //     for (let j = i + 1; j < array.length - 1; j++) {
+    // let pair = 0
+
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length - 1; j++) {
 
 //         if (array[i] + array[j] == 6) {
-//             count = count + 1;
+//             pair = pair + 1;
 //         }
 //     }
 
 // }
-// console.log(count)
+// console.log(pair)
 
 function TargetSum(x) {
-    let count = 0
+    let pair = 0
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length - 1; j++) {
 
             if (array[i] + array[j] == x) {
-                count = count + 1;
+                pair = pair + 1;
             }
         }
 
     }
-    return count
+    return pair
 }
 let result1 = TargetSum(7)
 let result2 = TargetSum(9)
