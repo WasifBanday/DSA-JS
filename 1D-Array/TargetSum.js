bc4889a9282ed410a03be72dc353002c4ee9f35b
wasif_manzoor <wasifahmad679@gmail.com>
2025-11-07 20:57:26 +0530

    TargetSUm

---
+++ 1D-Array/TargetSum.js
@@ -0,0 +1,14 @@
+// Find the total number of pairs in the Array whose sum is equal to the given value 'x'  ?
+
+let array = [2, 3, 4, 5, 6, 7, 8, 9]
+let count = 0
+for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length - 1; j++) {
+
+        if (array[i] + array[j] == 6) {
+            count = count + 1;
+        }
+    }
+
+}
+console.log(count)
